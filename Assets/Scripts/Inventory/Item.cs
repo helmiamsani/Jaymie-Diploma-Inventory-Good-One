ab2dc0db0d26ae7c425ff8b3f32695eefdf52ad1
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-10-14 15:59:45 +1100

    Boom

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class Item
+{
+    #region Private Variables
+    private int _id, _amount, _value;
+    private ItemType _type;
+    private string _name, _description;
+
+    private Texture2D _icon;
+    private GameObject _mesh;
+
+    private int _damage, _armour, _heal;
+    #endregion
+
+    #region Public Properties
+    public int ID
+    {
+        get { return _id; } //Read
+        set { _id = value; } //Write
+    }
+
+    public int Amount
+    {
+        get { return _amount; }
+        set { _amount = value; }
+    }
+
+    public int Value
+    {
+        get { return _value; }
+        set { _value = value; }
+    }
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public string Description
+    {
+        get { return _description; }
+        set { _description = value; }
+    }
+
+    public ItemType Type
+    {
+        get { return _type; }
+        set { _type = value; }
+    }
+
+    public Texture2D Icon
+    {
+        get { return _icon; }
+        set { _icon = value; }
+    }
+
+    public GameObject Meshtype
+    {
+        get { return _mesh; }
+        set { _mesh = value; }
+    }
+
+    public int Damage
+    {
+        get { return _damage; }
+        set { _damage = value; }
+    }
+
+    public int Armour
+    {
+        get { return _armour; }
+        set { _armour = value; }
+    }
+
+    public int Heal
+    {
+        get { return _heal; }
+        set { _heal = value; }
+    }
+    #endregion
+}
+
+public enum ItemType
+{
+    Ingredient,
+    Potion,
+    Scroll,
+    Food,
+    Armour,
+    Weapon,
+    Craftable,
+    Money,
+    Quest,
+    Misc
+}
---
+++ Assets/Scripts/Inventory/ItemData.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+
+public static class ItemData
+{
+    public static Item CreateItem(int itemID)
+    {
+        string _name = "";
+        string _description = "";
+        int _amount = 0;
+        int _value = 0;
+        ItemType _type = ItemType.Money;
+        string _icon = "";
+        string _mesh = "";
+        int _damage = 0;
+        int _armour = 0;
+        int _heal = 0;
+
+        switch (itemID)
+        {
+            case 0:
+                _name = "Acorn";
+                _description = "Good for eating.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Ingredient;
+                _icon = "Ingredient/Acorn";
+                _mesh = "Ingredient/Acorn";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+            case 1:
+                _name = "Iceroom";
+                _description = "Good for reheating body.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Ingredient;
+                _icon = "Ingredient/Iceroom";
+                _mesh = "Ingredient/Iceroom";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+            case 2:
+                _name = "Bark";
+                _description = "Good for burning.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Ingredient;
+                _icon = "Ingredient/Bark";
+                _mesh = "Ingredient/Bark";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+            case 3:
+                _name = "Energizer";
+                _description = "Drink it!! and you'll see what it does.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Potion;
+                _icon = "Potion/Energizer";
+                _mesh = "Potion/Energizer";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+            case 4:
+                _name = "Red Scroll";
+                _description = "Usually can be achieved from Royal Family.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Scroll;
+                _icon = "Scroll/Red Scroll";
+                _mesh = "Scroll/Red Scroll";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+            case 5:
+                _name = "Traveller Torso";
+                _description = "Popular among traveller.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Armour;
+                _icon = "Armour/Traveller Torso";
+                _mesh = "Armour/Traveller Torso";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+            case 6:
+                _name = "Hero Sword";
+                _description = "Dragons can be sliced in a second.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Weapon;
+                _icon = "Weapon/Hero Sword";
+                _mesh = "Weapon/Hero Sword";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+            case 7:
+                _name = "Human Brain";
+                _description = "First of all, Why do you have human brain?, Sell this brain to Town Macuni and get 200 Triangle of Gold.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Craftable;
+                _icon = "Craftable/Human Brain";
+                _mesh = "Craftable/Human Brain";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+            case 8:
+                _name = "State Bronze";
+                _description = "The lowest currency in Boganga Nation.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Money;
+                _icon = "Money/State Bronze";
+                _mesh = "Money/State Bronze";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+            case 9:
+                _name = "Eye of Manousa";
+                _description = "Manousa is a combination of horse, dragon, lion and tiger. Last time was seen around Dimitry Forest, all the Gejust lives there.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Quest;
+                _icon = "Quest/Eye of Manousa";
+                _mesh = "Quest/Eye of Manousa";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+            case 10:
+                _name = "Sheet";
+                _description = "A flexible rest equipment. Can be used for sleeping and rest during travel.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Misc;
+                _icon = "Misc/Sheet";
+                _mesh = "Misc/Sheet";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+
+            default:
+                itemID = 0;
+                _name = "Acorn";
+                _description = "Good for eating.";
+                _amount = 0;
+                _value = 0;
+                _type = ItemType.Ingredient;
+                _icon = "Ingredient/Acorn";
+                _mesh = "Ingredient/Acorn";
+                _damage = 0;
+                _armour = 0;
+                _heal = 0;
+                break;
+        }
+
+        Item temp = new Item
+        {
+            ID = itemID,
+            Name = _name,
+            Description = _description,
+            Amount = _amount,
+            Value = _value,
+            Type = _type,
+            Icon = Resources.Load("Icons/" + _icon) as Texture2D,
+            Meshtype = Resources.Load("Mesh/" + _mesh) as GameObject,
+            Damage = _damage,
+            Armour = _armour,
+            Heal = _heal
+        };
+        return temp;
+    }
+}
